fe6614b1b4d96d58864b8db815b7427791aa1a72
hnu459 <119164691+hnu459@users.noreply.github.com>
2022-12-18 21:20:11 +0200

    Update test_items.py

--- lesson3_6_step10/test_items.py
+++ lesson3_6_step10/test_items.py
@@ -1,6 +1,6 @@
 from selenium.webdriver.common.by import By
       
-def test_item(browser):  
+def test_add_to_cart_button_is_displayed(browser):  
     browser.get("http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/")
     button = browser.find_elements(By.CSS_SELECTOR, "button.btn-add-to-basket")
     assert button, "Page has no button add to cart."
